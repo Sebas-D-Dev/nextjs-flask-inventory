88de24de3ab33ebb404ebf6e879d2af737c6dde4
Sebas-D-Dev <sebas.t.nait@gmail.com>
2025-06-26 18:08:49 -0400

    deployment update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import './styles/globals.css';
-import './styles/modal.css';
+import '../styles/globals.css';
+import '../styles/modal.css';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
